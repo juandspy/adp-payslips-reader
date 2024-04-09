078cb8b138db6d15f29d81f0de1077d05e3d4af6
Juan Diaz Suarez <jdiazsua@redhat.com>
2024-04-09 21:30:39 +0200

    fix some bugs and improve the testing to cover all my year

--- main.py
+++ main.py
@@ -41,15 +41,15 @@ def _get_lines(filepath: str, page: int = 0) -> str:
 def get_main_concepts(filepath: str) -> MainConcepts:
     """Get the concepts from the main table."""
 
-    def parse_line(line: str) -> Tuple[int, str, float, float, float, float]:
+    def parse_line(line: str, _format: str = "new") -> Tuple[int, str, float, float, float, float]:
         code_and_concept = line[:29]
 
         codigo = _parse_int(code_and_concept[:4])
         concepto = code_and_concept[4:]
-        cantidad_o_base = _parse_float(line[29:40])
-        precio_uni = _parse_float(line[40:53])
-        devengos = _parse_float(line[53:67])
-        deducciones = _parse_float(line[67:])
+        cantidad_o_base = _parse_float(line[29:40], 1 if parse_format == "new" else 1000)
+        precio_uni = _parse_float(line[40:53], 1 if parse_format == "new" else 10000)
+        devengos = _parse_float(line[53:63], 1 if parse_format == "new" else 100)
+        deducciones = _parse_float(line[63:], 1 if parse_format == "new" else 100)
 
         return codigo, concepto, cantidad_o_base, \
             precio_uni, devengos, deducciones
@@ -57,7 +57,11 @@ def get_main_concepts(filepath: str) -> MainConcepts:
     out = MainConcepts()
 
     parse = False
-    for line in _get_lines(filepath):
+    parse_format = "new"
+    lines = _get_lines(filepath)
+    if "TRANSFERENCIA" in "".join(lines):
+        parse_format = "old"
+    for line in lines:
         # Ignore all lines until one that contains "SALARIO BASE"
         if "SALARIO BASE" in line:
             parse = True
@@ -67,7 +71,7 @@ def get_main_concepts(filepath: str) -> MainConcepts:
             continue
         if not re.search('[a-zA-Z0-9]', line):
             break
-        codigo, concepto, cantidad_o_base, precio_uni, devengos, deducciones = parse_line(line)
+        codigo, concepto, cantidad_o_base, precio_uni, devengos, deducciones = parse_line(line, parse_format)
 
         out.codigo.append(codigo)
         out.concepto.append(concepto)
@@ -81,69 +85,83 @@ def get_main_concepts(filepath: str) -> MainConcepts:
 def get_totales(filepath: str) -> Totals:
     out = Totals()
 
-    previous_non_empty_line = ""
-    for line in _get_lines(filepath):
+    line_with_totals = ""
+    lines = _get_lines(filepath)
+    divide_by = 1
+
+    for i, line in enumerate(lines):
         if "LIQUIDO A RECIBIR" in line:
             out.liquido_a_recibir = _parse_float(line.replace("€", "").\
                 replace(",", ".").replace("LIQUIDO A RECIBIR", ""))
+            line_with_totals = lines[i - 1]
+            break
 
-            splitted = previous_non_empty_line.\
-                strip().split(" ")
-            out.devengos = _parse_float(splitted[0])
-            out.deducciones = _parse_float(splitted[-1])
+        if "TRANSFERENCIA" in line:
+            out.liquido_a_recibir = _parse_float(lines[i - 2])
+            line_with_totals = lines[i - 5]
+            divide_by = 100
+    splitted = line_with_totals.strip().split(" ")
+    out.devengos = _parse_float(splitted[0], divide_by)
+    out.deducciones = _parse_float(splitted[-1], divide_by)
+    return out
 
-            return out
-        if len(line.strip()) > 0:
-            previous_non_empty_line = line
 
 def get_bases(filepath: str) -> Bases:
     out = Bases()
 
-    parse = False
     lines_to_parse = []
-    previous_line = ""
-    for line in _get_lines(filepath):
+    lines = _get_lines(filepath)
+
+    for i, line in enumerate(lines):
+        if "TRANSFERENCIA" in line:
+            lines_to_parse.append(lines[i-1])
+            lines_to_parse.extend(lines[i+2:i+11])
         if "****" in line:
             # The data is in the 5 next non empty lines after the bank account
             # and the line before!
             # The bank account is anonymized using some "*", so this is a dirty
             # trick to find the line
-            parse = True
-            lines_to_parse.append(previous_line)
-            continue
-        previous_line = line
-        if not parse:
-            continue
-        if len(lines_to_parse) == 6:
+            lines_to_parse.append(lines[i-1])
+            lines_to_parse.extend(lines[i+2:i+11])
             break
-        if len(line.strip()) == 0:
-            continue
-        lines_to_parse.append(line)
 
+    lines_to_parse = [line for line in lines_to_parse if len(line.strip()) != 0]
     out.calculo_bases = ["REMUNERACION PRORRATEO", "TOTAL", "BASE NORMALIZ."]
-    out.contin_comunes = [
-        _parse_float(x) for x in lines_to_parse[0].split('Banco')[0]\
-            .split(" ") if x != ""]
-    out.accidentes = [
-        _parse_float(x) for x in lines_to_parse[1].split(" ") if x != ""]
-    out.irpf = [
-        _parse_float(x) for x in lines_to_parse[2].split(" ") if x != ""]
+
+    remuneracion_prorrateo = [x for x in lines_to_parse[0].split(" ") if x != ""]
+    out.contin_comunes.append(_parse_float(remuneracion_prorrateo[0]))
+    out.accidentes.append(_parse_float(remuneracion_prorrateo[1]))
+    out.irpf.append(_parse_float(remuneracion_prorrateo[2]))
+
+    total = [_parse_float(x) for x in lines_to_parse[1].split(" ") if x != ""]
+    out.contin_comunes.append(total[0])
+    out.accidentes.append(total[1])
+    out.irpf.append(total[2])
+
+    base_normaliz = [x for x in lines_to_parse[2].split(" ") if x != ""]
+    out.contin_comunes.append(_parse_float(base_normaliz[0]))
+    out.accidentes.append(_parse_float(base_normaliz[1]))
+    out.irpf.append(_parse_float(base_normaliz[2]))
+
     out.acu_base_irpf = _parse_float(lines_to_parse[3])
     out.acu_irpf = _parse_float(lines_to_parse[4])
     out.acu_cotiz_ss = _parse_float(lines_to_parse[5])
+
     return out
 
 def _parse_int(s: str) -> int | None:
     return int(s.strip())
 
-def _parse_float(s: str) -> float | None:
+def _parse_float(s: str, divide_by:int = 1) -> float | None:
+    # in the old format there are no decimals, so we need to divide by 100, 1000 or 10000 depending on the column
     if len(s.strip()) == 0:
         return None
-    return round(float(s.replace(",", ".").strip()), 2)
+    return round(float(s.replace(",", ".").strip()) / divide_by, 2)
+
 
 
 if __name__ == "__main__":
-    filepath = os.environ.get("FILE_PATH", "example.pdf")
+    filepath = os.environ.get("FILE_PATH", "payslips/2023_01.pdf")
     print(get_main_concepts(filepath))
     print(get_totales(filepath))
     print(get_bases(filepath))
--- test_main.py
+++ test_main.py
@@ -1,52 +1,68 @@
 # pylint: disable=E1101
+import pytest
+import os
+
 from main import get_main_concepts, get_totales, get_bases
 
+MAIN_MAXIMUM_VALUE = 15000
+BASES_MAXIMUM_VALUE = 200000
+MIN_NUM_OF_CONCEPTS = 8
+PAYSLIPS_FOLDER = "payslips"
 
-INPUT = "example.pdf"
+@pytest.mark.parametrize("test_input", [
+    (f"{PAYSLIPS_FOLDER}/{x}") for x in os.listdir(PAYSLIPS_FOLDER)])
+class TestAllFormats:
 
-def test_main_concepts():
-    out = get_main_concepts(INPUT)
+    def test_main_concepts(self, test_input):
+        out = get_main_concepts(test_input)
 
-    for field in out.__dataclass_fields__:
-        v = getattr(out, field)
-        assert len(v) > 10
-        if field == "concepto" or field == "codigo":
-            continue
-        assert _check_all_are_floats_or_none(v)
-        assert any([x for x in v if x is not None])
-
-
-def test_totales():
-    out = get_totales(INPUT)
-
-    for field in out.__dataclass_fields__:
-        v = getattr(out, field)
-        assert v > 0
-        assert isinstance(v, float)
-    assert out.devengos - out.deducciones == out.liquido_a_recibir
+        for field in out.__dataclass_fields__:
+            v = getattr(out, field)
+            assert len(v) > MIN_NUM_OF_CONCEPTS
+            if field == "concepto" or field == "codigo":
+                continue
+            assert _check_all_are_floats_or_none(v)
+            assert any([x for x in v if x is not None])
+            assert _check_all_lower_than(v, MAIN_MAXIMUM_VALUE)
 
 
-def test_all():
-    main = get_main_concepts(INPUT)
-    totales = get_totales(INPUT)
+    def test_totales(self, test_input):
+        out = get_totales(test_input)
 
-    assert round(sum(filter(None, main.devengos)), 2) == totales.devengos
-    assert round(sum(filter(None, main.deducciones)), 2) == totales.deducciones
+        for field in out.__dataclass_fields__:
+            v = getattr(out, field)
+            assert v > 0
+            assert isinstance(v, float)
+            assert _check_all_lower_than([v], MAIN_MAXIMUM_VALUE)
+        assert round(out.devengos - out.deducciones, 2) == round(out.liquido_a_recibir, 2)
 
-def test_get_bases():
-    out = get_bases(INPUT)
 
-    assert len(out.calculo_bases) == 3
-    assert len(out.contin_comunes) == 3
-    assert len(out.accidentes) == 3
-    assert len(out.irpf) == 3
-    
-    assert _check_all_are_floats_non_zero(out.contin_comunes)
-    assert _check_all_are_floats_non_zero(out.accidentes)
-    assert _check_all_are_floats_non_zero(out.irpf)
+    def test_all(self, test_input):
+        main = get_main_concepts(test_input)
+        totales = get_totales(test_input)
+
+        assert round(sum(filter(None, main.devengos)), 2) == round(totales.devengos, 2)
+        assert round(sum(filter(None, main.deducciones)), 2) == round(totales.deducciones, 2)
+
+    def test_get_bases(self, test_input):
+        out = get_bases(test_input)
+
+        assert len(out.calculo_bases) == 3
+        for v in [out.contin_comunes, out.accidentes, out.irpf]:
+            assert len(v) == 3
+            assert _check_all_are_floats_non_zero(v)
+            assert _check_all_lower_than(v, BASES_MAXIMUM_VALUE)
 
 def _check_all_are_floats_non_zero(lst):
     return all(isinstance(i, float) and i > 0 for i in lst)
 
 def _check_all_are_floats_or_none(lst):
     return all((isinstance(i, float) and i > 0) or i is None for i in lst)
+
+def _check_all_lower_than(lst, target):
+    for i in lst:
+        if i is None:
+            continue
+        if i > target:
+            return False
+    return True
